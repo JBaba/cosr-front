66dd778010d76958429022b059ac9ba05f164339
JBaba <naimish101.viradia@gmail.com>
2016-04-10 14:03:14 -0400

    Test case modified

--- tests/webdriver/keyboard.js
+++ tests/webdriver/keyboard.js
@@ -5,13 +5,17 @@ describe('keyboard navigation', function() {
     it('tab & arrow keys from STATIC page', function* () {
 
         yield openSearchUrl({"q": "apple", "g": "en"});
+
+        // TAB should select the Search Input
+        yield browser.keys(["TAB","TAB","TAB","TAB","TAB"]);
+
         var focused = yield inspectElement(yield browser.elementActive());
         assert.equal(focused.tag, "body");
 
         // Arrow down should select the first result.
-        // yield browser.keys(["ARROW_DOWN"]);
-        // assert.equal(focused.tag, "a");
-        // assert.ok(focused.text.indexOf("Apple") >= 0);
+        yield browser.keys(["ARROW_DOWN"]);
+        assert.equal(focused.tag, "a");
+        assert.ok(focused.text.indexOf("Apple") >= 0);
 
     });
 
