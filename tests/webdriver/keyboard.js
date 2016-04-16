88a195c10eb56355188ece9cdd64ed36cc03c124
JBaba <naimish101.viradia@gmail.com>
2016-04-16 18:01:07 -0400

    Test of getName attribute

--- tests/webdriver/keyboard.js
+++ tests/webdriver/keyboard.js
@@ -1,15 +1,32 @@
 describe('keyboard navigation', function() {
-
 	// Key codes: https://seleniumhq.github.io/selenium/docs/api/py/webdriver/selenium.webdriver.common.keys.html
 
     it('tab & arrow keys from STATIC page', function* () {
 
         yield openSearchUrl({"q": "apple", "g": "en"});
 
+        console.log("Test started..")
         // TAB should select the Search Input
-        yield browser.keys(["TAB","TAB","TAB","TAB","TAB"]);
+        yield browser.keys(["TAB","a","TAB","TAB","TAB"]);
 
         var focused = yield inspectElement(yield browser.elementActive());
+        var ele = yield browser.elementActive();
+        //ele = ele.value.ELEMENT;
+        eleValue = browser.getAttribute(ele,'id');
+        var ele2 = browser.getValue('#q');
+
+        yield sleep(1000);
+        console.log(ele2)
+        console.log(ele)
+        console.log('Present Test:');
+        browser.elementIdName(elt.value.ELEMENT).then(function(res) {
+            console.log("Id value:"+res.value);
+        });
+        console.log('My test:');
+        browser.elementIdAttribute(elt.value.ELEMENT,'id').then(function(res) {
+            console.log("Id value:"+res.value);
+        });
+
         assert.equal(focused.tag, "a");
 
         // Arrow down should select the first result.
